86c6e6ea7a74ead6523d216704efbd6cd2785c6a
Simon Murray <spjmurray@yahoo.co.uk>
2025-05-22 12:41:25 +0100

    Infrastructure Health Monitoring (#65)
    OpenStack (or any cloud provide for that matter) can and will break,
    especially if the wind blows in Scotland... We have a new resource
    health API, so we may as well add in some polling to ensure things are
    in a good state.  This adds in the boilerplate.

---
+++ cmd/unikorn-compute-monitor/main.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2024-2025 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/pflag"
+
+	unikornv1 "github.com/unikorn-cloud/compute/pkg/apis/unikorn/v1alpha1"
+	"github.com/unikorn-cloud/compute/pkg/constants"
+	"github.com/unikorn-cloud/compute/pkg/monitor"
+	coreclient "github.com/unikorn-cloud/core/pkg/client"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func main() {
+	// Initialize components with legacy flags.
+	zapOptions := &zap.Options{}
+	zapOptions.BindFlags(flag.CommandLine)
+
+	// Initialize components with flags, then parse them.
+	monitorOptions := &monitor.Options{}
+	monitorOptions.AddFlags(pflag.CommandLine)
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	// Get logging going first, log sinks will expect JSON formatted output for everything.
+	log.SetLogger(zap.New(zap.UseFlagOptions(zapOptions)))
+
+	logger := log.Log.WithName(constants.Application)
+
+	// Hello World!
+	logger.Info("service starting", "application", constants.Application, "version", constants.Version, "revision", constants.Revision)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Register a signal handler to trigger a graceful shutdown.
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, syscall.SIGTERM)
+
+	go func() {
+		<-stop
+
+		// Cancel anything hanging off the root context.
+		cancel()
+	}()
+
+	client, err := coreclient.New(ctx, unikornv1.AddToScheme)
+	if err != nil {
+		logger.Error(err, "failed to create client")
+
+		return
+	}
+
+	monitor.Run(ctx, client, monitorOptions)
+}
